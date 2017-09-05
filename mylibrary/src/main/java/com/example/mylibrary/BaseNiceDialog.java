b58a44138963c883356f77caed7fee05d61abd5d
guhongliang <ghl1149660196@163.com>
2017-09-05 17:57:31 +0800

    add custom control view

--- mylibrary/src/main/java/com/example/mylibrary/BaseNiceDialog.java
+++ mylibrary/src/main/java/com/example/mylibrary/BaseNiceDialog.java
@@ -1,29 +1,159 @@
 package com.example.mylibrary;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
+import android.support.annotation.StyleRes;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by guhongliang on 2017/9/5.
  */
 
-public abstract class BaseNiceFrament extends DialogFragment {
+public abstract class BaseNiceDialog extends DialogFragment {
+    private static final String MARGIN = "margin";
+    private static final String WIDTH = "width";
+    private static final String HEIGHT = "height";
+    private static final String DIM = "dim_amount";
+    private static final String BOTTOM = "show_bottom";
+    private static final String CANCEL = "out_cancel";
+    private static final String ANIM = "anim_style";
+    private static final String LAYOUT = "layout_id";
+    private int width;//宽度
+    private int height;//高度
+    private int margin;//左右距离
+    private float dimAmount = 0.5f;//灰度深浅
+    private boolean showBottom;//是否底部显示
+    private boolean outCancel = true;//是否点击外部取消
+    @StyleRes
+    private int animStyle;
+    @LayoutRes
+    protected int layoutId;
+
+    public abstract void convertView(ViewHolder viewHolder, BaseNiceDialog dialog);
+
     public abstract int intLayoutId();
-    public abstract void  converView(ViewHolder viewHolder,);
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setStyle(DialogFragment.STYLE_NO_TITLE, R.style.NiceDialog);
+        layoutId = intLayoutId();
+        //恢复保存的数据
+        if (savedInstanceState != null) {
+            margin = savedInstanceState.getInt(MARGIN);
+            width = savedInstanceState.getInt(WIDTH);
+            height = savedInstanceState.getInt(HEIGHT);
+            dimAmount = savedInstanceState.getFloat(DIM);
+            showBottom = savedInstanceState.getBoolean(BOTTOM);
+            outCancel = savedInstanceState.getBoolean(CANCEL);
+            animStyle = savedInstanceState.getInt(ANIM);
+            layoutId = savedInstanceState.getInt(LAYOUT);
+        }
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(intLayoutId(), container, false);
+        View view = inflater.inflate(layoutId, container, false);
+        convertView(ViewHolder.create(view), this);
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initParams();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(MARGIN, margin);
+        outState.putInt(WIDTH, width);
+        outState.putInt(HEIGHT, height);
+        outState.putFloat(DIM, dimAmount);
+        outState.putBoolean(BOTTOM, showBottom);
+        outState.putBoolean(CANCEL, outCancel);
+        outState.putInt(ANIM, animStyle);
+        outState.putInt(LAYOUT, layoutId);
+    }
+
+    public void initParams() {
+        Window window = getDialog().getWindow();
+        if (window != null) {
+            WindowManager.LayoutParams params = window.getAttributes();
+            //调节灰色背景透明度[0-1]，默认为0.5f
+            params.dimAmount = dimAmount;
+            //是否在底部显示
+            if (showBottom) {
+                params.gravity = Gravity.BOTTOM;
+                if (animStyle == 0) {
+                    animStyle = R.style.DefaultAnimation;
+                }
+            }
+            //设置dialog大小
+            if (width == 0) {
+                params.width = Utils.getScreenWidth(getContext()) - 2 * Utils.dp2px(getContext(), margin);
+            } else {
+                params.width = Utils.dp2px(getContext(), width);
+            }
+            if (height == 0) {
+                params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            } else {
+                params.height = Utils.dp2px(getContext(), height);
+            }
+            window.setWindowAnimations(animStyle);
+            window.setAttributes(params);
+        }
+        setOutCancel(outCancel);
+    }
+
+    public BaseNiceDialog show(FragmentManager manager) {
+        super.show(manager, String.valueOf(System.currentTimeMillis()));
+        return this;
+    }
+
+    public BaseNiceDialog setWidth(int width) {
+        this.width = width;
+        return this;
+    }
+
+    public BaseNiceDialog setHeight(int height) {
+        this.height = height;
+        return this;
+    }
+
+    public BaseNiceDialog setMargin(int margin) {
+        this.margin = margin;
+        return this;
+    }
+
+    public BaseNiceDialog setDimAmount(float dimAmount) {
+        this.dimAmount = dimAmount;
+        return this;
+    }
+
+    public BaseNiceDialog setShowBottom(boolean showBottom) {
+        this.showBottom = showBottom;
+        return this;
+    }
+
+    public BaseNiceDialog setOutCancel(boolean outCancel) {
+        this.outCancel = outCancel;
+        return this;
+    }
+
+    public BaseNiceDialog setAnimStyle(int animStyle) {
+        this.animStyle = animStyle;
+        return this;
     }
 }
