e02664fd211866d45729267a7b58d858a8a47552
guhongliang <ghl1149660196@163.com>
2017-09-22 11:49:44 +0800

    test failed

--- bargraph/src/main/java/com/example/bargraph/Human.java
+++ bargraph/src/main/java/com/example/bargraph/Human.java
@@ -1,8 +1,16 @@
 package com.example.bargraph;
 
+import java.util.Observable;
+import java.util.Observer;
+
 /**
  * Created by guhongliang on 2017/9/22.
+ * 被观察者
  */
 
-public class Human {
+public class Human implements Observer {
+    @Override
+    public void update(Observable observable, Object o) {
+
+    }
 }
